b69b088c0148b2e5d7dbceb059433ff8b71ae7cc
¬xÅV«í Hao-Heng Hung <hhhung@linux1.cs.nctu.edu.tw>
2015-11-11 10:24:43 +0800

    all four children can pass their result to parent using four different pipe, so that parent can also distinguish the result of one child from another.

--- fork.c
+++ fork.c
@@ -2,11 +2,13 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/types.h>
+#include<fcntl.h>
+#include<string.h>
 
 void read_map_and_determine_the_size(char(*)[2000][2001],unsigned*,unsigned*) ;
 void determine_each_block_size_and_replace_the_wall_symbol(char(*)[2000][2001],const unsigned,const unsigned,unsigned*,unsigned*) ;
 void print_the_map(char(*)[2000][2001],const unsigned,const unsigned) ;
-void fork_four_miners(char(*)[2000][2001],const unsigned,const unsigned,const unsigned,const unsigned) ;
+void fork_four_miners(char(*)[2000][2001],const unsigned,const unsigned,const unsigned,const unsigned,unsigned(*)[4]) ;
     unsigned miner1(char(*)[2000][2001],const unsigned,const unsigned,const unsigned,const unsigned) ;
     unsigned miner2(char(*)[2000][2001],const unsigned,const unsigned,const unsigned,const unsigned) ;
     unsigned miner3(char(*)[2000][2001],const unsigned,const unsigned,const unsigned,const unsigned) ;
@@ -14,6 +16,7 @@ void fork_four_miners(char(*)[2000][2001],const unsigned,const unsigned,const un
 
 int main(){
     unsigned i , width , height , col_num_of_vertical_wall , row_num_of_horizontal_wall ;
+    unsigned miners[4] ;
     char map[2000][2001] ;
     
     read_map_and_determine_the_size(&map,&width,&height) ;
@@ -25,7 +28,7 @@ int main(){
 
     //printf("%d,%d\n",col_num_of_vertical_wall,row_num_of_horizontal_wall) ;
 
-    fork_four_miners(&map,width,height,col_num_of_vertical_wall,row_num_of_horizontal_wall) ;
+    fork_four_miners(&map,width,height,col_num_of_vertical_wall,row_num_of_horizontal_wall,&miners) ;
 
 
 }
@@ -86,10 +89,23 @@ void print_the_map(char(* m)[2000][2001],const unsigned w,const unsigned h){
 }
 
 
-void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,const unsigned col,const unsigned row){
+void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,const unsigned col,const unsigned row,unsigned (*miners)[4]){
     
     pid_t pid ;
     unsigned m1,m2,m3,m4 ;
+
+    int pipe1[2] ;
+    int pipe2[2] ;
+    int pipe3[2] ;
+    int pipe4[2] ;
+    pipe(pipe1) ;
+    pipe(pipe2) ;
+    pipe(pipe3) ;
+    pipe(pipe4) ;
+    char msgbuffer1[10] ;
+    char msgbuffer2[10] ;
+    char msgbuffer3[10] ;
+    char msgbuffer4[10] ;
 
     pid=fork();
     if(pid<0){
@@ -98,6 +114,9 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
     }
     else if(pid==0){
         m1=miner1(m,0,0,row,col) ;
+        char str[10] ;
+        sprintf(str,"%d",m1) ;
+        write(pipe1[1],str,strlen(str)+1) ;
     }
     else{
         pid=fork() ;
@@ -107,6 +126,9 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
         }
         else if(pid==0){
             m2=miner2(m,0,col+1,row,w) ;
+            char str[10] ;
+            sprintf(str,"%d",m2) ;
+            write(pipe2[1],str,strlen(str)+1) ;
         }
         else{
             pid=fork() ;
@@ -116,6 +138,9 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
             }
             else if(pid==0){
                 m3=miner3(m,row+1,0,h,col) ;
+                char str[10] ;
+                sprintf(str,"%d",m3) ;
+                write(pipe3[1],str,strlen(str)+1) ;
             }
             else{
                 pid=fork() ;
@@ -125,6 +150,9 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
                 }
                 else if(pid==0){
                     m4=miner4(m,row+1,col+1,h,w) ;
+                    char str[10] ;
+                    sprintf(str,"%d",m4) ;
+                    write(pipe4[1],str,strlen(str)+1) ;
                 }
                 else{
                     wait(NULL) ;
@@ -133,6 +161,15 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
                     wait(NULL) ;
                     //printf("%d %d %d %d\n",m1,m2,m3,m4) ;
                     //printf("all miners are here!\n") ;
+                    read(pipe1[0],msgbuffer1,10) ;
+                    read(pipe2[0],msgbuffer2,10) ;
+                    read(pipe3[0],msgbuffer3,10) ;
+                    read(pipe4[0],msgbuffer4,10) ;
+                    printf("%s\n",msgbuffer1) ;
+                    printf("%s\n",msgbuffer2) ;
+                    printf("%s\n",msgbuffer3) ;
+                    printf("%s\n",msgbuffer4) ;
+
                 }
             }
         }
