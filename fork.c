c425eac2ec668e7289b9b0424b4ed6567364474e
¬xÅV«í Hao-Heng Hung <hhhung@linux1.cs.nctu.edu.tw>
2015-11-11 13:51:37 +0800

    add some comment.

--- fork.c
+++ fork.c
@@ -10,9 +10,6 @@ void determine_each_block_size_and_replace_the_wall_symbol(char(*)[2000][2001],c
 void print_the_map(char(*)[2000][2001],const unsigned,const unsigned) ;
 void fork_four_miners(char(*)[2000][2001],const unsigned,const unsigned,const unsigned,const unsigned,unsigned(*)[4]) ;
     unsigned miner1(char(*)[2000][2001],const unsigned,const unsigned,const unsigned,const unsigned) ;
-    unsigned miner2(char(*)[2000][2001],const unsigned,const unsigned,const unsigned,const unsigned) ;
-    unsigned miner3(char(*)[2000][2001],const unsigned,const unsigned,const unsigned,const unsigned) ;
-    unsigned miner4(char(*)[2000][2001],const unsigned,const unsigned,const unsigned,const unsigned) ;
 void compare_and_print_the_result(unsigned(*)[4]) ;
 
 int main(){
@@ -39,9 +36,9 @@ int main(){
 
 void read_map_and_determine_the_size(char(* m)[2000][2001], unsigned* _w, unsigned* _h){
     unsigned i , w , h ;
-    for(h=0 ; scanf("%s", (*m)[h] )!=EOF ; h++){
+    for(h=0 ; scanf("%s", (*m)[h] )!=EOF ; h++){ // calculate  map height
     }
-    for(w=0 ; (*m)[0][w]=='.' || (*m)[0][w]=='*' ||(*m)[0][w]=='#' ; w++ ){
+    for(w=0 ; (*m)[0][w]=='.' || (*m)[0][w]=='*' ||(*m)[0][w]=='#' ; w++ ){ // calculate matrix width
     }
     
     *_w = w ;
@@ -50,17 +47,17 @@ void read_map_and_determine_the_size(char(* m)[2000][2001], unsigned* _w, unsign
 
 void determine_each_block_size_and_replace_the_wall_symbol(char(*m)[2000][2001],const unsigned w,const unsigned h,unsigned* _col,unsigned* _row){
     unsigned i , col , row ;
-    for(i=0 ; i<w && ( (*m)[0][i]=='.' || (*m)[0][i]=='*' ) ; i++){
+    for(i=0 ; i<w && ( (*m)[0][i]=='.' || (*m)[0][i]=='*' ) ; i++){ // find the first '|' symbol, i.e. the vertical wall.
     }
-    col = i ;
-    for(i=0 ; i<h ; i++){
+    col = i ; // col is the index of the vertical wall.
+    for(i=0 ; i<h ; i++){ // replace the whole vertical '#' symbol with '|'symbol.
         (*m)[i][col]='|' ;
     }
 
-    for(i=0 ; i<h && w>=2 && !( (*m)[i][0]=='#' && (*m)[i][0]=='#' ) ; i++ ){
+    for(i=0 ; i<h && w>=2 && !( (*m)[i][0]=='#' && (*m)[i][0]=='#' ) ; i++ ){ // find the first '#' symbol, i.e. the horizontal wall. 
     }
-    row = i ;
-    for(i=0 ; i<w ; i++){
+    row = i ; // row is the index of the horizontal wall.
+    for(i=0 ; i<w ; i++){ // replace '#' symbol with '-'
         (*m)[row][i]='-' ;
     }
 
@@ -98,10 +95,10 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
     pid_t pid ;
     unsigned m1,m2,m3,m4 ;
 
-    int pipe1[2] ;
-    int pipe2[2] ;
-    int pipe3[2] ;
-    int pipe4[2] ;
+    int pipe1[2] ; // for child 1
+    int pipe2[2] ; // for child 2
+    int pipe3[2] ; // for child 3
+    int pipe4[2] ; // for child 4
     pipe(pipe1) ;
     pipe(pipe2) ;
     pipe(pipe3) ;
@@ -119,9 +116,9 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
     else if(pid==0){
         m1=miner1(m,0,0,row,col) ;
         char str[10] ;
-        sprintf(str,"%d",m1) ;
-        write(pipe1[1],str,strlen(str)+1) ;
-        exit(0) ;
+        sprintf(str,"%d",m1) ; // convert unsigned integer to string.
+        write(pipe1[1],str,strlen(str)+1) ; // write the result into pipe for parent to retrieve.
+        exit(0) ; // do not forget this exit command!! or child will return to main function and continue to execute.
     }
     else{
         pid=fork() ;
@@ -130,7 +127,7 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
             exit(1) ;
         }
         else if(pid==0){
-            m2=miner2(m,0,col+1,row,w) ;
+            m2=miner1(m,0,col+1,row,w) ;
             char str[10] ;
             sprintf(str,"%d",m2) ;
             write(pipe2[1],str,strlen(str)+1) ;
@@ -143,7 +140,7 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
                 exit(1) ;
             }
             else if(pid==0){
-                m3=miner3(m,row+1,0,h,col) ;
+                m3=miner1(m,row+1,0,h,col) ;
                 char str[10] ;
                 sprintf(str,"%d",m3) ;
                 write(pipe3[1],str,strlen(str)+1) ;
@@ -156,7 +153,7 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
                     exit(1) ;
                 }
                 else if(pid==0){
-                    m4=miner4(m,row+1,col+1,h,w) ;
+                    m4=miner1(m,row+1,col+1,h,w) ;
                     char str[10] ;
                     sprintf(str,"%d",m4) ;
                     write(pipe4[1],str,strlen(str)+1) ;
@@ -166,10 +163,10 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
                     wait(NULL) ;
                     wait(NULL) ;
                     wait(NULL) ;
-                    wait(NULL) ;
+                    wait(NULL) ; // wait for all four children finishing executing.
                     //printf("%d %d %d %d\n",m1,m2,m3,m4) ;
                     //printf("all miners are here!\n") ;
-                    read(pipe1[0],msgbuffer1,10) ;
+                    read(pipe1[0],msgbuffer1,10) ; // read the result from the four children.
                     read(pipe2[0],msgbuffer2,10) ;
                     read(pipe3[0],msgbuffer3,10) ;
                     read(pipe4[0],msgbuffer4,10) ;
@@ -177,7 +174,7 @@ void fork_four_miners(char(*m)[2000][2001],const unsigned w,const unsigned h,con
                     //printf("%s\n",msgbuffer2) ;
                     //printf("%s\n",msgbuffer3) ;
                     //printf("%s\n",msgbuffer4) ;
-                    (*miners)[0] = atoi(msgbuffer1) ;
+                    (*miners)[0] = atoi(msgbuffer1) ; // convert the result back to unsigned integer.
                     (*miners)[1] = atoi(msgbuffer2) ;
                     (*miners)[2] = atoi(msgbuffer3) ;
                     (*miners)[3] = atoi(msgbuffer4) ;
@@ -204,47 +201,6 @@ unsigned miner1(char(*m)[2000][2001],const unsigned x1,const unsigned y1,const u
 }
 
 
-unsigned miner2(char(*m)[2000][2001],const unsigned x1,const unsigned y1,const unsigned x2,const unsigned y2){
-    //printf("miner2!\n") ;
-    unsigned i , j , count=0 ;
-    for(i=y1 ; i<y2 ; i++){
-        for(j=x1 ; j<x2 ; j++){
-            //printf("miner2 %c\n",(*m)[j][i]) ;
-            if((*m)[j][i]=='*') count++ ;
-        }
-    }
-    //printf("miner2 %d\n",count) ;
-    return count ;
-}
-
-
-unsigned miner3(char(*m)[2000][2001],const unsigned x1,const unsigned y1,const unsigned x2,const unsigned y2){
-    //printf("miner3!\n") ;
-    unsigned i , j , count=0 ;
-    for(i=y1 ; i<y2 ; i++){
-        for(j=x1 ; j<x2 ; j++){
-            //printf("miner3 %c\n",(*m)[j][i]) ;
-            if((*m)[j][i]=='*') count++ ;
-        }
-    }
-    //printf("miner3 %d\n",count) ;
-    return count ;
-}
-
-
-
-unsigned miner4(char(*m)[2000][2001],const unsigned x1,const unsigned y1,const unsigned x2,const unsigned y2){
-    //printf("miner4!\n") ;
-    unsigned i , j , count=0 ;
-    for(i=y1 ; i<y2 ; i++){
-        for(j=x1 ; j<x2 ; j++){
-            //printf("miner4 %c\n",(*m)[j][i]) ;
-            if((*m)[j][i]=='*') count++ ;
-        }
-    }
-    //printf("miner4 %d\n",count) ;
-    return count ;
-}
 
 void compare_and_print_the_result(unsigned (*miners)[4]){
     unsigned max=(*miners)[0] ;
